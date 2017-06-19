9f5d61a9b21fe7a995562c391e900cfbef5af3fd
gacfrs <gacfrs@gmail.com>
2017-06-19 02:28:07 -0300

    Update Tracker.ino

--- Tracker.ino
+++ Tracker.ino
@@ -1,110 +1,111 @@
-
 /****************************************************************************
     EA076 C - Projeto Final - Tracker.io
     Dimitri Reis        RA 145869
     Guilherme Frauches  RA 155591
     Placa solar que se ajusta de acordo com a incidencia luminosa e
-    mostra a potencia gerada pela placa em um display lcd
+    mostra a tensao gerada pela placa em um display lcd
  ****************************************************************************/
 
 #include <Servo.h>
-//defining Servos
-Servo servohori;
-int servoh = 90;
-int servoLimitHigh = 120;
-int servoLimitLow = 60;
-
-#define faixa 100
-
-Servo servoverti;
-int servov = 90;
-//Assigning LDRs
-const int ldrleft = A0;    // Pino referente ao LDR da esquerda
-const int ldrup = A1;    // Pino referente ao LDR de cima
-const int ldrright = A2;   // Pino referente ao LDR da direita
-const int ldrdown = A3;
-
 #include <LiquidCrystal.h>
-//Define os pinos que serão utilizados para ligação ao display
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-int portasolar = A5;
-float tensao_placa = 0;
-void setup ()
-{
-  //Define o número de colunas e linhas do LCD
-  lcd.begin(16, 2);
 
-  servohori.attach(10);
+Servo servohori;    // Define o servo horizontal
+Servo servoverti;   // Define o servo vertical
+
+// Define os pinos que serão utilizados para ligação ao display
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+#define servoLimitHigh 120  // Limites superior e inferior dos servos
+#define servoLimitLow 60
+#define band 100            // Faixa de valor aceitavel para os LDRs
+
+int servoh = 90;
+int servov = 90;
+float voltage = 0;          // Tensao lida pelo painel solar
+
+const int ldrleft = A0;     // Define os pinos dos LDRs e painel solar
+const int ldrup = A1;
+const int ldrright = A2;
+const int ldrdown = A3;
+const int solarpanel = A5;
+
+void setup () {
+  lcd.begin(16, 2);       // Define o numero de colunas e linhas do LCD
+
+  servohori.attach(10);   // Define portas e posicoes iniciais dos servos
   servohori.write(90);
   servoverti.attach(6);
   servoverti.write(90);
+
   delay(500);
   Serial.begin(9600);
 }
 
-void loop()
-{
-  servoh = servohori.read();
+void loop() {
+  servoh = servohori.read();              // Le as posicoes atuais dos servos
   servov = servoverti.read();
-  //capturing analog values of each LDR
-  int up = analogRead(ldrup);
+
+  int up = analogRead(ldrup);             // Le os valores dos LDRs
   int down = analogRead(ldrdown) + 70;
   int left = analogRead(ldrleft) + 70;
   int right = analogRead(ldrright);
-  // calculating average
-  int dif_hor = abs(left - right);
+
+  int dif_hor = abs(left - right);        // Calcula a diferenca absoluta entre eles
   int dif_ver = abs(up - down);
 
-  if (dif_ver >= faixa) {
-    if (up > down) { //gira pra esquerda
-
+  if (dif_ver >= band) {                  // Se a diferenca for maior que a faixa de erro aceitavel os motores regulam o painel
+    if (up > down) {
       if (servov >= servoLimitHigh)
         servov = servoLimitHigh;
       servoverti.write(servov + 10);
       delay(10);
-    } else if (down > up) {
+    }
+
+    else if (down > up) {
       if (servov <= servoLimitLow)
         servov = servoLimitLow;
       servoverti.write(servov - 10);
       delay(10);
-    } else
+    }
+
+    else
       servoverti.write(servov);
+    delay(50);
   }
 
-  if (dif_hor >= faixa) {
-    if (left > right) { //gira pra esquerda
-
+  if (dif_hor >= band) {
+    if (left > right) {
       if (servoh <= servoLimitLow)
         servoh = servoLimitLow;
       servohori.write(servoh - 10);
       delay(10);
     }
-    else if (right > left)  {
 
+    else if (right > left)  {
       if (servoh >= servoLimitHigh)
         servoh = servoLimitHigh;
       servohori.write(servoh + 10);
       delay(10);
-    } else
+    }
+
+    else
       servohori.write(servoh);
     delay(50);
   }
 
-  tensao_placa = analogRead(portasolar);
-  tensao_placa = map (tensao_placa, 0, 630, 0, 300);
-  tensao_placa = tensao_placa / 100;
-  //Limpa a tela
-  lcd.clear();
-  //Posiciona o cursor na coluna 3, linha 0;
-  lcd.setCursor(0, 0);
-  //Envia o texto entre aspas para o LCD
-  lcd.print("Tensao: ");
-  lcd.print(tensao_placa);
+  voltage = analogRead(solarpanel);         // Le a tensao do painel solar
+  voltage = map (voltage, 0, 630, 0, 300);  // Mapeia entre seus extremos
+  voltage = voltage / 100;
+
+  lcd.clear();                // Limpa a tela
+  lcd.setCursor(0, 0);        // Posiciona o cursor na coluna 0, linha 0
+  lcd.print("Tensao: ");      // Imprime o texto no LCD
+  lcd.print(voltage);
   lcd.setCursor(0, 1);
   lcd.print("Capacid.: ");
-  tensao_placa = tensao_placa * 33.33;
-  lcd.print(tensao_placa);
+  voltage = voltage * 33.33;
+  lcd.print(voltage);
   lcd.print("%");
-  
+
   delay(500);
 }
